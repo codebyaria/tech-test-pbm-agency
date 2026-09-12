6bea4e203cf7fbf9f532669415b2b4c59047f1c2
codebyaria <68700786+codebyaria@users.noreply.github.com>
2026-09-12 19:22:24 +0700

    fix: select Apache MPM at container startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,4 +14,7 @@ php artisan optimize --no-interaction
 sed -ri "s!Listen 80!Listen ${PORT}!" /etc/apache2/ports.conf
 sed -ri "s!<VirtualHost \*:80>!<VirtualHost *:${PORT}>!" /etc/apache2/sites-available/000-default.conf
 
+a2dismod mpm_event mpm_worker >/dev/null 2>&1 || true
+a2enmod mpm_prefork >/dev/null 2>&1
+
 exec apache2-foreground
